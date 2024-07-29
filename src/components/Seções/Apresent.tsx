56f351ea94e71b38a7b251082af3676a70efdaa0
Anthoni Campos <anthonithomes@gmail.com>
2024-07-29 19:43:22 -0300

    feature: TypeIt adicionado

--- src/components/Seções/Apresent.tsx
+++ src/components/Seções/Apresent.tsx
@@ -1,12 +1,29 @@
+"use client";
+
 import Image from "next/image";
 import React from "react";
+import TypeIt from "typeit-react";
 
 const Apresent = () => {
   return (
     <>
       <section className="flex flex-col items-center w-full justify-center pt-28">
         <div className="flex gap-4 text-7xl font-bold">
-          Desenvolvedor <p className="text-roxo">Front-End</p>
+          Desenvolvedor
+          <p className="text-roxo">
+            <TypeIt
+              options={{ loop: true }}
+              getBeforeInit={(instance) => {
+                instance
+                  .type("Front-End")
+                  .pause(2500)
+                  .delete()
+                  .pause(2500)
+                  .type("Web");
+                return instance;
+              }}
+            />
+          </p>
         </div>
         <p className="text-3xl">Foco em resultados e designs responsivos</p>
       </section>
@@ -15,20 +32,20 @@ const Apresent = () => {
         <Image
           src="/assets/pattern1.png"
           alt="pattern1"
-          width={200}
-          height={200}
-        />
-        <Image
-          src="/assets/pattern3.png"
-          alt="pattern1"
           width={300}
           height={300}
         />
         <Image
+          src="/assets/pattern3.png"
+          alt="pattern1"
+          width={500}
+          height={500}
+        />
+        <Image
           src="/assets/pattern4.png"
           alt="pattern1"
-          width={200}
-          height={200}
+          width={300}
+          height={300}
         />
       </div>
     </>
