95fd94512dd50a26be586fb3b9d80baad009bbcd
Anthoni Campos <anthonithomes@gmail.com>
2024-08-07 20:00:13 -0300

    feature: Responsividade mobile

--- src/components/Seções/Contato.tsx
+++ src/components/Seções/Contato.tsx
@@ -10,17 +10,19 @@ const Contato = () => {
       <section className="flex flex-col w-full pt-20 items-center absolute">
         <div className="flex w-full items-center justify-center gap-10">
           <div className="border-b-2 w-[20%]"></div>
-          <div className="text-4xl font-bold flex gap-2">Contato</div>
+          <div className="md:text-4xl text-2xl font-bold">Contato</div>
           <div className="border-b-2 w-[20%]"></div>
         </div>
 
-        <div className="w-full justify-center flex py-10 px-[5%]">
+        <div className="w-full justify-center flex py-10 md:px-[5%]">
           <div className="border-2 border-roxo w-[80%] rounded-xl p-10 gap-10 flex flex-col items-center">
-            <p className="font-bold text-3xl">Vamos construir algo juntos!</p>
+            <p className="font-bold md:text-3xl text-xl text-center md:text-start">
+              Vamos construir algo juntos!
+            </p>
 
             <form className="w-full flex flex-col gap-5 items-center">
               <section className="flex flex-col gap-2 w-full ">
-                <p className="text-xl font-bold">Qual o seu nome?</p>
+                <p className="md:text-xl font-bold">Qual o seu nome?</p>
                 <input
                   className="rounded-lg h-8 text-black outline-none"
                   type="text"
@@ -30,7 +32,7 @@ const Contato = () => {
               </section>
 
               <section className="flex flex-col gap-2 w-full ">
-                <p className="text-xl font-bold">Email para contato:</p>
+                <p className="md:text-xl font-bold">Email para contato:</p>
                 <input
                   className="rounded-lg h-8 text-black outline-none"
                   type="text"
@@ -40,7 +42,9 @@ const Contato = () => {
               </section>
 
               <section className="flex flex-col gap-2 w-full ">
-                <p className="text-xl font-bold">Me conte sobre sua ideia:</p>
+                <p className="md:text-xl font-bold">
+                  Me conte sobre sua ideia:
+                </p>
                 <textarea
                   className="rounded-lg h-28 text-black outline-none"
                   name="nome"
@@ -48,7 +52,7 @@ const Contato = () => {
                 />
               </section>
 
-              <button className="bg-roxo w-[25%] h-10 rounded-lg hover:bg-purple-900 duration-200">
+              <button className="bg-roxo md:w-[25%] w-[60%] h-10 rounded-lg hover:bg-purple-900 duration-200">
                 Enviar
               </button>
             </form>
@@ -58,14 +62,14 @@ const Contato = () => {
               alt="Pattern 5"
               width={300}
               height={300}
-              className="absolute left-0 z-[-10]"
+              className="absolute left-0 z-[-10] hidden md:flex"
             />
             <Image
               src="/assets/pattern2.png"
               alt="Pattern 2"
               width={200}
               height={200}
-              className="absolute right-0 translate-y-[-50%] z-[-10]"
+              className="absolute right-0 translate-y-[-50%] z-[-10] hidden md:flex"
             />
           </div>
         </div>
