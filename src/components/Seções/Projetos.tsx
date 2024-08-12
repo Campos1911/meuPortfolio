452d585f77342bf0b263058a74aa78cbbad1882b
Anthoni Campos <anthonithomes@gmail.com>
2024-08-11 21:44:58 -0300

    feature: Dois projetos adicionados

--- src/components/Seções/Projetos.tsx
+++ src/components/Seções/Projetos.tsx
@@ -1,9 +1,12 @@
 "use client";
 
-import React from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 import { Element } from "react-scroll";
 
 const Projetos = () => {
+  const router = useRouter();
   return (
     <Element name="projs">
       <section className="flex flex-col w-full px-2 pt-10 items-center">
@@ -13,10 +16,22 @@ const Projetos = () => {
           <div className="border-b-2 w-[20%]"></div>
         </div>
         <div className="grid md:grid-cols-2 pt-10 gap-5 w-[90%] lg:w-[80%]">
-          <div className="w-[100%] h-64 bg-gray-200 rounded-xl"></div>
-          <div className="w-[100%] h-64 bg-gray-200 rounded-xl"></div>
-          <div className="w-[100%] h-64 bg-gray-200 rounded-xl"></div>
-          <div className="w-[100%] h-64 bg-gray-200 rounded-xl"></div>
+          <Image
+            src="/assets/mardu.png"
+            alt="Mardu Cosméticos"
+            width={1000}
+            height={1000}
+            className="rounded-md hover:opacity-30 duration-200 cursor-pointer"
+            onClick={() => router.push("https://marducosmeticos.com/")}
+          ></Image>
+          <Image
+            src="/assets/paulo.png"
+            alt="Mardu Cosméticos"
+            width={1000}
+            height={1000}
+            className="rounded-md hover:opacity-30 duration-200 cursor-pointer"
+            onClick={() => router.push("https://dinheirofobia.com/")}
+          />
         </div>
       </section>
     </Element>
